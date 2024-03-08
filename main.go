1ce8a8c815f8326e8caf3574d4fdc02809aa6c20
jayyi <eyjian@qq.com>
2024-03-09 07:01:29 +0800

    优化错误打印

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
     ctx := context.Background()
     districtTable, err := district.LoadDistrict(ctx, "./district-2022.csv")
     if err != nil {
-        fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+        fmt.Fprintf(os.Stderr, "load district error: %s\n", err.Error())
     } else {
         jsonBytes, err := json.Marshal(*districtTable)
         if err != nil {
-            fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+            fmt.Fprintf(os.Stderr, "json marshal error: %s\n", err.Error())
         } else {
             fmt.Fprintf(os.Stdout, "%s\n", string(jsonBytes))
         }
